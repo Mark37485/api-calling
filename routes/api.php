27b67818b6692fe1161f40fac52d657a1330524a
Mark37485 <genosomark29@gmail.com>
2026-05-24 23:10:08 +0800

    final commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// GET - fetch all students
+Route::get('/students', [StudentsController::class, 'index']);
+
+// GET - fetch one student by ID
+Route::get('/students/{id}', [StudentsController::class, 'show']);
+
+// POST - create a new student
+Route::post('/students', [StudentsController::class, 'store']);
+
+// PUT - update an existing student by ID
+Route::put('/students/{id}', [StudentsController::class, 'update']);
+
+// PATCH - partial update of an existing student by ID
+Route::patch('/students/{id}', [StudentsController::class, 'patch']);
+
+// DELETE - delete all students
+Route::delete('/students', [StudentsController::class, 'destroyAll']);
+// DELETE - delete one student by ID
+Route::delete('/students/{id}', [StudentsController::class, 'destroy']);
+
